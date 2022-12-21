da203402dad3b994bd42137d062c6539709478bb
Ramon <ramon.guimera@skyscanner.net>
2022-12-21 18:11:50 +0100

    show available and all regions

--- AWholeS/__main__.py
+++ AWholeS/__main__.py
@@ -1,3 +1,4 @@
+from AWholeS.ec2 import show_all_regions, show_all_available_regions
 from AWholeS.sts_calls import get_current_arn, get_current_assumed_role, get_current_account_id
 from AWholeS.trusted_advisor_security import show_failed_security_checks, show_failed_security_checks_with_account_id
 
@@ -13,7 +14,14 @@ if __name__ == "__main__":
     print("Your current AssumedRole is:")
     print(get_current_assumed_role())
     print("\n")
+    print("Showing all regions:")
+    show_all_regions()
+    print("\n")
+    print("Showing current available regions:")
+    show_all_available_regions()
+    print("\n")
     print(f"Current Security failures for your account {get_current_account_id()}:")
     print("\n")
-    print(show_failed_security_checks())
-    print(show_failed_security_checks_with_account_id(account_id=get_current_account_id()))
+    show_failed_security_checks()
+    print("\n")
+    show_failed_security_checks_with_account_id(account_id=get_current_account_id())
---
+++ AWholeS/ec2.py
@@ -0,0 +1,23 @@
+import boto3
+
+
+def show_all_available_regions():
+    """
+    Prints current available regions
+
+    :return: None
+    """
+    client = boto3.client('ec2')
+    for region in client.describe_regions()['Regions']:
+        print(region["RegionName"])
+
+
+def show_all_regions():
+    """
+    Prints current available regions
+
+    :return: None
+    """
+    client = boto3.client('ec2')
+    for region in client.describe_regions(AllRegions=True)['Regions']:
+        print(region["RegionName"])
