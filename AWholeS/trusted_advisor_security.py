521ec5ef3cbc5f36df219e382371ebff14107e09
Ramon <ramon.guimera@skyscanner.net>
2022-12-22 10:11:32 +0100

    created interactive menu and improved TrustedAdvisor as class

--- AWholeS/trusted_advisor_security.py
+++ AWholeS/trusted_advisor_security.py
@@ -1,4 +1,97 @@
+from dataclasses import dataclass
+
 import boto3
+
+
+@dataclass
+class FailedSecurityChecks:
+    error_checks: list
+    warning_checks: list
+
+
+class TrustedAdvisor:
+
+    def __init__(self):
+        self._support_client = boto3.client('support', region_name='us-east-1')
+
+    def describe_checks(self):
+        """
+        Prints information about Trusted Advisor Security Checks
+
+        :return: None
+        """
+        try:
+            response = self._support_client.describe_trusted_advisor_checks(language='en')
+            for check in response.get("checks"):
+                if check.get("category") == "security":
+                    print(f"{check.get('id')} - {check.get('name')} - {check.get('metadata')}")
+        except Exception as e:
+            print(e)
+
+    def obtain_security_checks(self) -> list:
+        """
+        Returns the entire list of Trusted Advisor Security checks
+
+        :return: list
+        """
+        security_checks = []
+        try:
+            response = self._support_client.describe_trusted_advisor_checks(language='en')
+            for check in response.get("checks"):
+                if check.get("category") == "security":
+                    security_checks.append(check)
+        except Exception as e:
+            print(e)
+        return security_checks
+
+    def describe_check_by_check_id(self, check_id: str):
+        """
+        Prints the response of describing a given check by its id
+
+        :param check_id: Id of the check
+        :return: None
+        """
+        try:
+            response = self._support_client.describe_trusted_advisor_check_result(checkId=check_id, language='en')
+            print(response)
+        except Exception as e:
+            print(e)
+
+    def obtain_failed_security_checks(self) -> FailedSecurityChecks:
+        """
+        Returns a list of all failed with error Trusted Advisor Security Checks
+
+        :return: None
+        """
+        error_security_checks = []
+        warning_security_checks = []
+        for sec_check in self.obtain_security_checks():
+            try:
+                response = self._support_client.describe_trusted_advisor_check_result(checkId=sec_check.get("id"),
+                                                                                      language='en')
+                check_status = response.get("result").get("status")
+                if check_status in ["error"]:
+                    error_security_checks.append(sec_check)
+                if check_status in ["warning"]:
+                    warning_security_checks.append(sec_check)
+            except Exception as e:
+                print(e)
+
+        return FailedSecurityChecks(error_checks=error_security_checks, warning_checks=warning_security_checks)
+
+    def show_failed_security_checks(self):
+        """
+        Prints failed Trusted Advisor Security Checks
+
+        :return: None
+        """
+        failed_checks = self.obtain_failed_security_checks()
+        print("ERROR")
+        for sec_check in failed_checks.error_checks:
+            print(sec_check.get("name"))
+        print("WARNING")
+        for sec_check in failed_checks.warning_checks:
+            print(sec_check.get("name"))
 
 
 def describe_severity_levels():
@@ -12,116 +105,3 @@ def describe_severity_levels():
         language='en'
     )
     print(response)
-
-
-def describe_checks():
-    """
-    Prints information about Trusted Advisor Security Checks
-
-    :return: None
-    """
-    try:
-        client = boto3.client('support', region_name='us-east-1')
-        response = client.describe_trusted_advisor_checks(language='en')
-        for check in response.get("checks"):
-            if check.get("category") == "security":
-                print(f"{check.get('id')} - {check.get('name')} - {check.get('metadata')}")
-    except Exception as e:
-        print(e)
-
-
-def obtain_security_checks() -> list:
-    """
-    Returns the entire list of Trusted Advisor Security checks
-
-    :return: list
-    """
-    security_checks = []
-    try:
-        client = boto3.client('support', region_name='us-east-1')
-        response = client.describe_trusted_advisor_checks(language='en')
-        for check in response.get("checks"):
-            if check.get("category") == "security":
-                security_checks.append(check)
-    except Exception as e:
-        print(e)
-    return security_checks
-
-
-def describe_check_by_check_id(check_id: str):
-    """
-    Prints the response of describing a given check by its id
-
-    :param check_id: Id of the check
-    :return: None
-    """
-    try:
-        client = boto3.client('support', region_name='us-east-1')
-        response = client.describe_trusted_advisor_check_result(checkId=check_id, language='en')
-        print(response)
-    except Exception as e:
-        print(e)
-
-
-def obtain_failed_security_checks() -> list:
-    """
-    Returns a list of all failed with error Trusted Advisor Security Checks
-
-    :return: None
-    """
-    failed_security_checks = []
-    client = boto3.client('support', region_name='us-east-1')
-
-    for sec_check in obtain_security_checks():
-        try:
-            response = client.describe_trusted_advisor_check_result(checkId=sec_check.get("id"), language='en')
-            check_status = response.get("result").get("status")
-            if check_status in ["error"]:
-                failed_security_checks.append(sec_check)
-        except Exception as e:
-            print(e)
-    return failed_security_checks
-
-
-def obtain_warning_security_checks() -> list:
-    """
-    Returns a list of all failed with warning Trusted Advisor Security Checks
-
-    :return: None
-    """
-    warning_security_checks = []
-    client = boto3.client('support', region_name='us-east-1')
-
-    for sec_check in obtain_security_checks():
-        try:
-            response = client.describe_trusted_advisor_check_result(checkId=sec_check.get("id"), language='en')
-            check_status = response.get("result").get("status")
-            if check_status in ["warning"]:
-                warning_security_checks.append(sec_check)
-        except Exception as e:
-            print(e)
-    return warning_security_checks
-
-
-def show_failed_security_checks():
-    """
-    Prints failed Trusted Advisor Security Checks
-
-    :return: None
-    """
-    print("FAILED")
-    for sec_check in obtain_failed_security_checks():
-        print(sec_check.get("name"))
-    print("WARNING")
-    for sec_check in obtain_warning_security_checks():
-        print(sec_check.get("name"))
-
-
-def show_failed_security_checks_with_account_id(account_id: str):
-    """
-    Prints failed Trusted Advisor Security Checks
-
-    :return: None
-    """
-    for sec_check in obtain_failed_security_checks():
-        print(f"{account_id} - {sec_check.get('name')}")
