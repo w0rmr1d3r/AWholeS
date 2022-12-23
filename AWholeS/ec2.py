b0ab9c2099a08b01d4db9de28141c2cfb0daebc9
Ramon <ramon.guimera@skyscanner.net>
2022-12-23 21:24:24 +0100

    improve ec2 regions

--- AWholeS/ec2.py
+++ AWholeS/ec2.py
@@ -1,15 +1,22 @@
 import boto3
 
 
+def obtain_all_available_regions() -> list:
+    regions = []
+    client = boto3.client("ec2")
+    for region in client.describe_regions()["Regions"]:
+        regions.append(region["RegionName"])
+    return regions
+
+
 def show_all_available_regions():
     """
     Prints current available regions
 
     :return: None
     """
-    client = boto3.client("ec2")
-    for region in client.describe_regions()["Regions"]:
-        print(region["RegionName"])
+    for region in obtain_all_available_regions():
+        print(region)
 
 
 def show_all_regions():
